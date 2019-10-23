3b3bb275a83a85e7257a4144789cff4f62f046a1
greg <52139156+su-greg@users.noreply.github.com>
2019-10-23 21:02:42 +0100

    Update evaluate.cs

--- evaluate.cs
+++ evaluate.cs
@@ -14,14 +14,14 @@ namespace MathEval {
         
         
         // Function to evaluate maths
-        public static float EvalMath(string s) {
-            while (s.Contains('(')) { // Evaluate the contence of any brackets and then replace them
+        public static float EvalMath(string strExp) {
+            while (strExp.Contains('(')) { // Evaluate the contence of any brackets and then replace them
                     // Remove the brackets from the string                                               And replace with the evaluation of said brackets
-                s = s.Remove(s.IndexOf('('), s.LastIndexOf(')') - s.IndexOf('(') + 1).Insert(s.IndexOf('('), EvalMath(s.Substring(s.IndexOf('(') + 1, s.LastIndexOf(')') - s.IndexOf('(') - 1)).ToString());
+                strExp = strExp.Remove(strExp.IndexOf('('), strExp.LastIndexOf(')') - strExp.IndexOf('(') + 1).Insert(strExp.IndexOf('('), EvalMath(strExp.Substring(strExp.IndexOf('(') + 1, strExp.LastIndexOf(')') - strExp.IndexOf('(') - 1)).ToString());
             }
             
             // split the string into numbers and symbols for doing maths
-            string[] x = Regex.Split(s, @"([x\^\/]|(?<!E)[\+\-])");
+            string[] x = Regex.Split(strExp, @"([x*\^\/]|(?<!E)[\+\-])");
             
             // convert that into a list so we can use some cool functions
             List<string> parts = x.ToList();
@@ -31,7 +31,7 @@ namespace MathEval {
             while (i < parts.Count) { 
                 
                 // if the element is any operator (could use !float.TryParse instead probs)
-                if (parts[i] == "^" || parts[i] == "/" || parts[i] == "x" || parts[i] == "+" || parts[i] == "-") {
+                if (parts[i] == "^" || parts[i] == "/" || parts[i] == "x" || parts[i] == "+" || parts[i] == "-" || parts[i] == "*") {
                     
                     // Replace that element (operater) with the result of the calculation using the numbers either side of it
                     parts[i] = MathDis(parts[i], float.Parse(parts[i - 1]), float.Parse(parts[i + 1])).ToString();
@@ -57,6 +57,7 @@ namespace MathEval {
                 case "^": return (float)Math.Pow(x, y);
                 case "/": return x / y;
                 case "x": return x * y;
+                case "*": return x * y;
                 case "+": return x + y;
                 case "-": return x - y;
                 default: throw new Exception("invalid logic");
